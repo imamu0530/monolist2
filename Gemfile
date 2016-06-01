476ba7dd611356dd1916faee30342c60e88e556d
imamu0530 <imamura@eswell.co.jp>
2016-06-01 13:47:45 +0000

    kadai_shusei

--- Gemfile
+++ Gemfile
@@ -18,18 +18,18 @@ gem 'therubyracer', platforms: :ruby
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+#gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
 gem 'sdoc', '~> 0.4.0', group: :doc
 
+gem 'bootstrap-sass'
+
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
 
 gem 'dotenv-rails', :groups => [:development, :test]
-
-gem 'bootstrap-sass'
 
 # http://webservice.rakuten.co.jp/sdk/ruby.html
 # https://github.com/rakuten-ws/rws-ruby-sdk
--- app/controllers/rankings_controller.rb
+++ app/controllers/rankings_controller.rb
@@ -2,14 +2,17 @@ class RankingsController < ApplicationController
 
 
   def have
+    @have_key = Have.group(:item_id).order('count_id desc').limit(10).count(:id)
+    @items = @have_key.transform_keys!{ |key| Item.find(key) }
     #@items = current_user.have_items.limit(10)
-    @have_key = Have.group(:item_id).order('count_all desc').limit(10).count.keys
-    @items=Item.find(@have_key)
+    #@have_key = Have.group(:item_id).order('count_all desc').limit(10).count.keys
+    #@items=@have_key.map { |key| Item.key}
+    #@items=Item.where(id: @have_key)
   end
   
   def want
-    @have_key = Want.group(:item_id).order('count_all desc').limit(10).count.keys
-    @items = Item.find(@have_key)
+    @want_key = Want.group(:item_id).order('count_all desc').limit(10).count.keys
+    @items = Item.where(id: @want_key)
   end
 
 
